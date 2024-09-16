ac2a9aa0296f084685203947d22d28efb74c4801
Winston Shi <157433864+WinstonShi10@users.noreply.github.com>
2024-09-15 22:00:06 -0400

    Updated main file to do what Reflection #2 asks for

--- README.md
+++ README.md
@@ -2,19 +2,10 @@
 
 Basic OpenMP template
 
-## Getting started ##
-To compile and run from command line if you are not on windows:
-```
-cd src
-g++ -fopenmp main.cpp CStopWatch.cpp
-./a.out
-```
-or
-```
-cd Default && make all
-```
+## How to Setup Project ##
 
 To compile and run with Docker:
+
 ```
 docker run --rm -v ${PWD}:/tmp -w /tmp/Default rgreen13/alpine-bash-gpp make all
 docker run --rm -v ${PWD}:/tmp -w /tmp/Default rgreen13/alpine-bash-gpp ./OpenMP
--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,81 @@
+// Open multi-process library
 #include <omp.h>
+// Generate Random Numbers
 #include <random>
+// Adds input/output functionality using streams
 #include <iostream>
+// Adds a CPU time function
 #include "CStopWatch.h"
 
-double f(double x) {
-    return x * x; // Example function: f(x) = x^2
+
+// Returns the number squared
+double f(double x);
+// Calculate the area under a wave function using the Trapezoidal Rule
+double serialTrap(double a, double b, int n);
+
+
+int main(){
+    // Initialize a pseudorandom number generator
+    srandom(time(NULL));
+    // Initialize Variable
+    double result = 0;
+    double a, b;
+    int n;
+    // Initialize timer
+    CStopWatch timer;
+    // Reflection #2: Run The function with intervals a=0 and b=16
+    a = 0;
+    b = 16;
+    // Reflection #2: Get a value of 'n' ranging from 10,000 to 100,000 using steps of 10,000
+    for(n = 10000; n < 110000; n += 10000) {
+        // Reflection #2: Run 1 to 12 threads/processors
+        for(int thread_num = 1; thread_num < 13; thread_num++) {
+            // Setup OMP threads
+            omp_set_num_threads(thread_num);
+            // Specify the number of threads
+            #pragma omp parallel num_threads(thread_num)
+            // Start Timer
+            timer.startTimer();
+            // Call function for trapezoidal rule
+            result = serialTrap(a, b, n);
+            // Stop timer
+            timer.stopTimer();
+            // Output results
+            std::cout << "n        Threads     Result     Time" << std::endl;
+            std::cout << n << "   ";
+            std::cout << thread_num << "           ";
+            std::cout << result << "    ";
+            std::cout << timer.getElapsedTime() << std::endl;
+            // Reset Result
+            result = 0.0;
+        }
+        std::cout << "--------------------------------------" << std::endl;
+    }
+    return 0;
 }
 
+
+double f(double x) {
+    // Multiply the number by itself
+    return x * x;
+}
+
+
 double serialTrap(double a, double b, int n) {
-
+    // Initialize floating number variables
     double h, retValue, x;
-
+    // Calculate width interval
     h = (b - a) / n;
+    // Approximate initial area under the curve
     retValue = (f(a) + f(b)) / 2.0;
-
+    // Using parallelism, run the Trapezoidal Rule 'n' times
     #pragma omp parallel for reduction(+:retValue)
     for (int i = 1; i < n; i++) {
         x = a + i * h;
         retValue += f(x);
     }
-
+    // multiplying the accumulated area by the interval width
     retValue *= h;
+    // return the final area
     return retValue;
 }
-
-int main(){
-
-    double result = 0;
-    double a, b;
-    int n;
-    CStopWatch timer;
-
-    a = 2;
-    b = 14;
-    n = 40000;
-
-    #pragma omp parallel num_threads(1)
-
-    timer.startTimer();
-    result = serialTrap(a, b, n);
-    timer.stopTimer();
-    std::cout << "Serial Result: " << result << std::endl;
-    std::cout << "Serial Time:" << timer.getElapsedTime() << std::endl;
-
-    omp_set_num_threads(4);
-    result = 0.0;
-    
-    #pragma omp parallel num_threads(4)
-    timer.startTimer();
-    result = serialTrap(a, b, n);
-    timer.stopTimer();
-    std::cout << "Parallel Result: " << result << std::endl;
-    std::cout << "Parallel Time:" << timer.getElapsedTime() << std::endl;
-
-    return 0;
-}
-
-
